73463ec44bdd886b2b7ad6a6c9ae58eda78ada5d
Kevin <kevhines@gmail.com>
2021-04-21 14:26:12 -0400

    commit tree 708d9bb642c33b9ff2642467dcbfe09a345b35d7

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 100)
+
+let num3 = 39
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(1,20)
